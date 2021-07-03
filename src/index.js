19781c731e24c63e78400e86bf855f009487dfa3
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-07-03 18:09:05 +0900

    [Update]Resize

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-function resize() {
-    const place = document.getElementById("displayTime");
-    let count = document.getElementById("displayHour").textContent.length + document.getElementById("displayMin").textContent.length + document.getElementById("displaySec").textContent.length;
-    if (count < 7) {
-      count = 7;
-    }
-    if (window.innerWidth <= 775) {
-      place.style.fontSize = 150 / count + "vmin";
-    } else {
-      place.style.fontSize = 185 / count + "vmin";
-    }
-  }
+import {resize} from "./style";
   resize();
+  window.addEventListener("resize",resize,false);
